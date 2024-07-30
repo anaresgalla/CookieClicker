f05fe0fcdda486308a5b3b506cee0bab7f3fb033
anaresgalla <anaresgalla@gmail.com>
2024-07-30 17:19:55 -0300

    Adds banner to GUI

--- src/CookieClicker.java
+++ src/CookieClicker.java
@@ -1,5 +1,8 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
 
 public class CookieClicker extends JFrame {
     public CookieClicker(){
@@ -9,5 +12,34 @@ public class CookieClicker extends JFrame {
         pack();
         setResizable(false);
         setLocationRelativeTo(null);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents(){
+        SpringLayout springLayout = new SpringLayout();
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(springLayout);
+
+        // 1.Banner
+        JLabel bannerImg = loadImage("resources/banner.png");
+
+        jPanel.add(bannerImg);
+
+        this.getContentPane().add(jPanel);
+    }
+
+    private JLabel loadImage(String fileName){
+        BufferedImage image;
+        JLabel imageContainer;
+        try{
+            InputStream inputStream = this.getClass().getResourceAsStream(fileName);
+            image = ImageIO.read(inputStream);
+            imageContainer = new JLabel(new ImageIcon(image));
+            return imageContainer;
+        }catch(Exception e){
+            System.out.println("Error: " + e);
+            return null;
+        }
     }
 }
